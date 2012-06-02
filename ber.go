1e0ed1c8f1d925a447c999a759e2e049d9e51c5f
Joseph Crail <jbcrail@gmail.com>
2012-06-02 15:34:18 -0400

    Fix typos.

--- ber.go
+++ ber.go
@@ -98,13 +98,13 @@ var ClassMap = map[uint8]string{
 }
 
 const (
-	TypePrimative   = 0  // xx0xxxxxb
+	TypePrimitive   = 0  // xx0xxxxxb
 	TypeConstructed = 32 // xx1xxxxxb
 	TypeBitmask     = 32 // xx1xxxxxb
 )
 
 var TypeMap = map[uint8]string{
-	TypePrimative:   "Primative",
+	TypePrimitive:   "Primitive",
 	TypeConstructed: "Constructed",
 }
 
@@ -399,7 +399,7 @@ func Encode(ClassType, TagType, Tag uint8, Value interface{}, Description string
 }
 
 func NewSequence(Description string) *Packet {
-	return Encode(ClassUniversal, TypePrimative, TagSequence, nil, Description)
+	return Encode(ClassUniversal, TypePrimitive, TagSequence, nil, Description)
 }
 
 func NewBoolean(ClassType, TagType, Tag uint8, Value bool, Description string) *Packet {
